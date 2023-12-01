8830a5c9c9572fc46db23b14d08dfa4a8d3cbea7
NovusX-Dev <152214057+mhijaziB@users.noreply.github.com>
2023-12-01 09:43:20 -0300

    Game working
    - commented out Readme editor scripts
    - UI scale fix
    - update to 1.19.7
    - Unity version: 2020.3.48f1

--- client/Assets/Scripts/Editor/Beamable/Samples/BBB/AutoOpenReadme.cs
+++ client/Assets/Scripts/Editor/Beamable/Samples/BBB/AutoOpenReadme.cs
@@ -1,45 +1,45 @@
-﻿using Beamable.Samples.Core;
-using Beamable.Samples.SampleProjectBase;
-using UnityEditor;
-
-namespace Beamable.Samples.BBB
-{
-	/// <summary>
-	/// Automatically focus on the sample project readme file when Unity first opens.
-	/// </summary>
-	[CustomEditor(typeof(Readme))]
-	[InitializeOnLoad]
-	public class AutoOpenReadme : ReadmeEditor
-	{
-		private const string Title = "BBB Readme";
-		private const string FindAssetsFilter = "Readme t:Readme";
-		private const string SessionStateKeyWasAlreadyShown = "Beamable.Samples.BBB.AutoOpenReadme.wasAlreadyShown";
-		private static string[] FindAssetsFolders = new string[] { "Assets" };
-
-		static AutoOpenReadme()
-		{
-			EditorApplication.delayCall += SelectReadmeAutomatically;
-		}
-
-		private static void SelectReadmeAutomatically()
-		{
-			if (!SessionState.GetBool(SessionStateKeyWasAlreadyShown, false))
-			{
-				SelectSpecificReadmeMenuItem();
-				SessionState.SetBool(SessionStateKeyWasAlreadyShown, true);
-			}
-		}
-
-		[MenuItem(
-			BeamableConstants.MENU_ITEM_PATH_WINDOW_BEAMABLE_SAMPLES + "/Microservices/" +
-			BeamableConstants.OPEN + " " + Title,
-			priority = BeamableConstants.MENU_ITEM_PATH_WINDOW_PRIORITY_4)]
-		private static Readme SelectSpecificReadmeMenuItem()
-		{
-			// Reset SessionState if/when MenuItem is used
-			SessionState.SetBool(SessionStateKeyWasAlreadyShown, false);
-			return ReadmeEditor.SelectReadme(FindAssetsFilter, FindAssetsFolders);
-
-		}
-	}
-}
+﻿// using Beamable.Samples.Core;
+// using Beamable.Samples.SampleProjectBase;
+// using UnityEditor;
+//
+// namespace Beamable.Samples.BBB
+// {
+// 	/// <summary>
+// 	/// Automatically focus on the sample project readme file when Unity first opens.
+// 	/// </summary>
+// 	[CustomEditor(typeof(Readme))]
+// 	[InitializeOnLoad]
+// 	public class AutoOpenReadme : ReadmeEditor
+// 	{
+// 		private const string Title = "BBB Readme";
+// 		private const string FindAssetsFilter = "Readme t:Readme";
+// 		private const string SessionStateKeyWasAlreadyShown = "Beamable.Samples.BBB.AutoOpenReadme.wasAlreadyShown";
+// 		private static string[] FindAssetsFolders = new string[] { "Assets" };
+//
+// 		static AutoOpenReadme()
+// 		{
+// 			EditorApplication.delayCall += SelectReadmeAutomatically;
+// 		}
+//
+// 		private static void SelectReadmeAutomatically()
+// 		{
+// 			if (!SessionState.GetBool(SessionStateKeyWasAlreadyShown, false))
+// 			{
+// 				SelectSpecificReadmeMenuItem();
+// 				SessionState.SetBool(SessionStateKeyWasAlreadyShown, true);
+// 			}
+// 		}
+//
+// 		[MenuItem(
+// 			BeamableConstants.MENU_ITEM_PATH_WINDOW_BEAMABLE_SAMPLES + "/Microservices/" +
+// 			BeamableConstants.OPEN + " " + Title,
+// 			priority = BeamableConstants.MENU_ITEM_PATH_WINDOW_PRIORITY_4)]
+// 		private static Readme SelectSpecificReadmeMenuItem()
+// 		{
+// 			// Reset SessionState if/when MenuItem is used
+// 			SessionState.SetBool(SessionStateKeyWasAlreadyShown, false);
+// 			return ReadmeEditor.SelectReadme(FindAssetsFilter, FindAssetsFolders);
+//
+// 		}
+// 	}
+// }
